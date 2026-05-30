f64dcde670c0d8475112a6914cf903089928579e
FajarrKurniawan9 <fk357421@gmail.com>
2026-05-30 23:04:51 +0700

    feat: add user, student, organization, achievement, bill, and submission modules with controllers and services
    - Implemented User, Student, Organization, Achievement, Bill, and Submission modules.
    - Created corresponding controllers and services for each module.
    - Added DTOs for creating and updating entities.
    - Established relationships in the Prisma schema for users, students, organizations, achievements, bills, and submissions.
    - Added unit tests for controllers and services to ensure functionality.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentsModule } from './students/students.module';
+import { AuthModule } from './auth/auth.module';
+import { AchievementsModule } from './achievements/achievements.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { UsersModule } from './users/users.module';
+import { BillsModule } from './bills/bills.module';
+import { SubmissionsModule } from './submissions/submissions.module';
 
 @Module({
-  imports: [],
+  imports: [StudentsModule, AuthModule, AchievementsModule, OrganizationsModule, UsersModule, BillsModule, SubmissionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
