1709b01ebc28e6721146eea0c832c140f8c62126
FajarrKurniawan9 <fk357421@gmail.com>
2026-06-03 07:31:06 +0700

    fix: update CORS origin configuration to remove environment variable fallback

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      process.env.FRONTEND_URL ?? 'http://localhost:3000',
-      'https://app-askala.vercel.app/',
-    ],
+    origin: ['http://localhost:3000', 'https://app-askala.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
   });
